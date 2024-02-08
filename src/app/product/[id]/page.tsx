b10e44bf53b77ad9b481989d8ad6e54ef972c664
Sven Dockx <sven.dockx@gmail.com>
2024-02-08 16:45:47 +0100

    feat(add): product detail page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -13,15 +13,6 @@ export default function Index() {
     thumbnail:
       "https://images.unsplash.com/photo-1491756975177-a13d74ed1e2f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGZpZ2h0ZXJ8ZW58MHx8MHx8fDA%3D",
   };
-  const products = [
-    product,
-    product,
-    product,
-    product,
-    product,
-    product,
-    product,
-  ];
 
   return (
     <div className="min-h-screen flex justify-center">
@@ -29,9 +20,7 @@ export default function Index() {
         id="Projects"
         className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5"
       >
-        {products.map((prod, index) => {
-          return <Card key={index} product={prod} />;
-        })}
+        <Card product={product} />
       </section>
     </div>
   );
