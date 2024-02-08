c6df378898f06c23f04dc54ab7ea02c1c7033915
Sven Dockx <sven.dockx@gmail.com>
2024-02-08 12:36:18 +0100

    feat(add): register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -6,38 +6,36 @@ import { useState } from "react";
 
 export default function Login() {
   const [email, setEmail] = useState<string>();
+  const [firstname, setFirstname] = useState<string>();
+  const [lastname, setLastname] = useState<string>();
   const [password, setPassword] = useState<string>();
 
   const onSubmit = () => {
     console.log("call to api with params:");
     console.log(`email: ${email}`);
+    console.log(`firstname: ${firstname}`);
+    console.log(`lastname: ${lastname}`);
     console.log(`password: ${password}`);
   };
 
   return (
     <div className="flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
+      <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-3" action="#" method="POST">
+          <div className="flex gap-3 justify-between">
+            <Input title="Prénom" name="firstname" onChange={setFirstname} />
+            <Input title="Nom" name="lastname" onChange={setLastname} />
+          </div>
           <Input title="Email" name="email" onChange={setEmail} />
-          <Input
-            title="Mot de passe"
-            name="password"
-            link={{
-              title: "Mot de passe oublié",
-              href: "/",
-            }}
-            onChange={setPassword}
-          />
-
-          <Button title="Se connecter" onClick={onSubmit} />
+          <Input title="Mot de passe" name="password" onChange={setPassword} />
+          <Button title="S'inscrire" type="submit" onClick={onSubmit} />
         </form>
-
         <p className="mt-5 text-center text-sm text-gray-500">
           <a
             href="/"
             className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 ml-2"
           >
-            S&apos;incrire
+            Se connecter
           </a>
         </p>
       </div>
