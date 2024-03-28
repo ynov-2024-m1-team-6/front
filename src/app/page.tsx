23f87107041bb6ba41ab67b7833db9ad23628915
DarylNyd <95940306+DarylNyd@users.noreply.github.com>
2024-03-28 10:55:27 +0100

    feat(wip): modif home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import Navbar from "@/layout/navbar";
 import Footer from "@/layout/footer";
 import Image from "next/image";
 
+import Hero from "@/components/hero";
+import ModalUser from "@/components/modalNavbar";
+
 import HomePic from "p/img/home.jpg";
 import fight from "p/img/fight.png";
 
@@ -9,109 +12,26 @@ export default function Home() {
   return (
     <main>
       <Navbar />
-      <div className="flex flex-col items-center justify-center h-full">
-        <div className="flex flex-col justify-center w-4/5 py-10">
-          <h1 className="text-4xl font-bold text-center">
-            Welcome to Uber Bagare
-          </h1>
-          <p className="text-xl mt-4">
-            {" "}
+      <Hero
+            img={HomePic.src}
+            title="Sauvetage avec Uber Bagarre"
+            description="
             Bienvenue sur Uber Bagarre ! Louez des sportifs de haut de niveau
             pour des combats intenses et violents. Dominez l&apos;arène avec
             puissance et style. Vous n&apos;aurez plus jamais de probleme du a
             l&apos;harcelement parce que vous avez la flemme de soulevez des
-            charges a la salle , Contactez Nous et on Bagarre{" "}
-          </p>
-        </div>
-        <div className="flex flex-col items-center justify-center w-full">
-          <Image
-            className="flex-col rounded-md"
-            src={HomePic}
-            alt="home"
-            width={1100}
-            height={300}
-          />
-          <a href="/product" className="mt-10">
-            <div className="h-[50px] min-w-52 px-8 flex rounded-xl items-center text-white bg-black ">
-              <p>Decouvrez nos Combatants</p>
-            </div>
-          </a>
-        </div>
+            charges a la salle , Contactez Nous et on Bagarre"
+            link="/product"
+            link_text="Decouvrez nos Combatants"
+        />
+      <div className="flex flex-col items-center justify-center h-full">
         <p className="text-2xl text-center font-bold p-5 mt-10">
           {" "}
           Decouvrer nos combattants les plus populaires
         </p>
 
         <div className="flex flex-row gap-x-8 w-4/5 p-10 mb-8">
-          <div className="h-[250px] w-[300px] bg-gray-300 flex flex-col justify-center items-center rounded-2xl p-6">
-            <Image
-              className="rounded-xl"
-              src={fight}
-              alt="home"
-              width={300}
-              height={800}
-            />
-
-            <div className="flex text-white bg-black h-[40px] w-[100px] rounded-xl p-3 m-2 justify-center items-center">
-              <p>Fighter 1</p>
-            </div>
-          </div>
-
-          <div className="h-[250px] w-[300px] bg-gray-300 flex flex-col justify-center items-center rounded-2xl p-6">
-            <Image
-              className="rounded-xl"
-              src={fight}
-              alt="home"
-              width={300}
-              height={800}
-            />
-
-            <div className="flex text-white bg-black h-[40px] w-[100px] rounded-xl p-3 m-2 justify-center items-center">
-              <p>Fighter 1</p>
-            </div>
-          </div>
-
-          <div className="h-[250px] w-[300px] bg-gray-300 flex flex-col justify-center items-center rounded-2xl p-6">
-            <Image
-              className="rounded-xl"
-              src={fight}
-              alt="home"
-              width={300}
-              height={800}
-            />
-
-            <div className="flex text-white bg-black h-[40px] w-[100px] rounded-xl p-3 m-2 justify-center items-center">
-              <p>Fighter 1</p>
-            </div>
-          </div>
-
-          <div className="h-[250px] w-[300px] bg-gray-300 flex flex-col justify-center items-center rounded-2xl p-6">
-            <Image
-              className="rounded-xl"
-              src={fight}
-              alt="home"
-              width={300}
-              height={800}
-            />
-
-            <div className="flex text-white bg-black h-[40px] w-[100px] rounded-xl p-3 m-2 justify-center items-center">
-              <p>Fighter 1</p>
-            </div>
-          </div>
-
-          <div className="h-[250px] w-[300px] bg-gray-300 flex flex-col justify-center items-center rounded-2xl p-6">
-            <Image
-              className="rounded-xl"
-              src={fight}
-              alt="home"
-              width={300}
-              height={800}
-            />
-
-            <div className="flex text-white bg-black h-[40px] w-[100px] rounded-xl p-3 m-2 justify-center items-center">
-              <p>Fighter 1</p>
-            </div>
-          </div>
+          
         </div>
       </div>
       <Footer />
